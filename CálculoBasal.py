269cd7c7e8afbe5957190a6e3591257b04f3c697
TiagoDias333 <tfpdias@gmail.com>
2025-03-09 17:43:00 -0300

    Update CálculoBasal.py

--- CálculoBasal.py
+++ CálculoBasal.py
@@ -1,5 +1,8 @@
+from time import sleep
 peso = float(input("Digite seu peso:[kg] "))
 altura = int(input("Digite sua altura:[cm] "))
 idade = int(input("Digite sua idade: "))
 basal = (10*peso)+(6.25*altura)-(5.0*idade)
+print("CALCULANDO......")
+sleep(1)
 print(f'Seu metabolismo basal está em \33[93m{basal}\33[m calorias.')
